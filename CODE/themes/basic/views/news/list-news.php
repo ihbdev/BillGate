5bb956f4770d1bbe045b2ce24cf6e957ee25e691
unknown <user@user-DK.(none)>
2012-07-05 15:00:11 +0700

    2012/07/05

--- CODE/themes/basic/views/news/list-news.php
+++ CODE/themes/basic/views/news/list-news.php
@@ -4,19 +4,44 @@ $this->bread_crumbs=array(
 	array('url'=>Yii::app()->createUrl('news'),'title'=>Language::t('Tin tức')),
 )
 ?>
+<?php 
+$template='<div class="box">
+                <div class="box-title">
+                    <div class="page-button">
+                    	<span>{summary}</span>
+                       	{pager}
+                    </div>
+                </div>
+                <div class="box-content">
+                {items}
+                </div><!--box-content-->
+			</div><!--box-->';
+?>
+	<div class="box">
+		<div class="box-title">
+		<div class="box-content">
 			<div class="box">
-				<div class="box-title"><label>Tin tức</label></div>
+				<?php $this->widget('wBanner',array('code'=>Banner::CODE_HOMEPAGE,'view'=>'home-page'))?>
                 <div class="box-content">
                     <div class="news-list">
-		                <?php $this->widget('iPhoenixListView', array(
-							'dataProvider'=>$list_news,
-							'itemView'=>'_news',
-							'template'=>"{items}\n{pager}",
-		            		'pager'=>array('class'=>'iPhoenixLinkPager'),
-		            		'itemsCssClass'=>'news-list',
-		            		'pagerCssClass'=>'pages-inner',
-						)); 
-						?>
+						<?php $this->widget('iPhoenixListView', 
+							array(
+		            			'id'=>'list-product',
+								'dataProvider'=>$list_news,
+								'itemView'=>'_news',
+								'template'=>$template,	
+		            			'itemsCssClass'=>'news-list',
+		                   		'pagerCssClass'=>'pages-inner',
+		                   		'pager'=>array(
+		                   			'class'=>'iPhoenixLinkPager',
+		   							'prevPageLabel'=>'',
+		   							'nextPageLabel'=>'',        
+		                   			'maxButtonCount'=>0
+		                   		),
+		                   		'summaryText'=>'Trang {page}/{pages}'
+							)); ?>
 					</div>
 				</div><!--box-content-->
 			</div><!--box-->
+		</div>
+	</div><!--box-->
